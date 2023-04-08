763968d480c193e199eaa309c504a906fb860f8b
Yury Takzhanov <Yuriy.Takzhanov@rt.ru>
2023-04-08 13:27:39 +0300

    lesson-01 fix by 3th review, inject factory directly

--- lesson-01-question-reader/src/main/java/io/github/takzhanov/otus/spring/lesson01/service/OutputServiceImpl.java
+++ lesson-01-question-reader/src/main/java/io/github/takzhanov/otus/spring/lesson01/service/OutputServiceImpl.java
@@ -2,15 +2,16 @@ package io.github.takzhanov.otus.spring.lesson01.service;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class OutputServiceImpl implements OutputService {
-    private final OutputStream outputStream;
+    private final Supplier<OutputStream> outputStreamFactory;
 
     @Override
     public void println(String s) {
-        var printStream = new PrintStream(outputStream);
+        var printStream = new PrintStream(outputStreamFactory.get());
         printStream.println(s);
     }
 }
